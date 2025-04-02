a8d978eeec0c5879721199d2e7fb351d70d37da5
menicolas27 <menicolas@wpi.edu>
2025-04-02 16:14:18 -0400

    test 2

--- src/components/title.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-const Title: FC = () => (
-    <h1 className = "col text-center">
-        CS3733-D25 Team 39: Video Games & Electronic Repair
-    </h1>
-);
-
-export default Title;
